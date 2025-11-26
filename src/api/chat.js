159386f4c36be48b436105935a5b708e354a1eeb
Lee Jun Xian <jxlee0398@gmail.com>
2025-11-26 16:01:38 +0800

    env

--- src/api/chat.js
+++ src/api/chat.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const ENDPOINT = 'http://localhost:3000/message';
+export const ENDPOINT = import.meta.env.VITE_ENDPOINT || 'http://localhost:3000/message';
 
 export async function sendChatMessage({ userId, message }) {
   const res = await axios.post(ENDPOINT, { userId, message });
